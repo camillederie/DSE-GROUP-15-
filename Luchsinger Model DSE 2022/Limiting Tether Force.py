7ce0fafaf702e1817f6ab737afb3b8831b76b136
tswennink <tswennink@student.tudelft.nl>
2022-05-25 11:40:20 +0200

    Power Curve Power Limit

--- Luchsinger Model DSE 2022/Limiting Tether Force.py
+++ Luchsinger Model DSE 2022/Limiting Tether Force.py
@@ -4,15 +4,19 @@ from Optimal_gamma import *
 import matplotlib.pyplot as plt
 
 
-def calculate_tether_limit_reel_speeds():
+
+def calculate_tether_limit_reel_speeds(step):
     gamma_out_n,gamma_in_n = calculate_opt_gamma_nominal() #nominal reel out from optimal_gamma.py
 
-    v_w = np.linspace(v_w_n,2.5*v_w_n,100)
-    gamma_in = np.linspace(gamma_in_n, 0.25, 100)
-    gamma_out = np.linspace(gamma_out_n, 0.05, 100)
-    gamma_in_max_f_c = np.zeros(100)
-    gamma_out_max_f_c = np.zeros(100)
-    f_c_mu = np.zeros(100)
+    F_in = 0.07   
+    F_out = 5.4
+
+    v_w = np.linspace(v_w_n,2.5*v_w_n,step)
+    gamma_in = np.linspace(gamma_in_n, 0.25, step)
+    gamma_out = np.linspace(gamma_out_n, 0.05, step)
+    gamma_in_max_f_c = np.zeros(step)
+    gamma_out_max_f_c = np.zeros(step)
+    f_c_mu = np.zeros(step)
 
     ci = 0
     cj = 0
@@ -28,11 +32,11 @@ def calculate_tether_limit_reel_speeds():
         gamma_out_max_f_c[ci] = 1-((1-gamma_out_n)/mu)
         ci +=1
         cj = 0
-    print (gamma_in_max_f_c,gamma_out_max_f_c,v_w)
+    #print (gamma_in_max_f_c,gamma_out_max_f_c,v_w)
     return gamma_in_max_f_c,gamma_out_max_f_c,v_w
 
-def plot_tether_limit_reel_speeds():
-    gamma_in_max_f_c,gamma_out_max_f_c,v_w = calculate_tether_limit_reel_speeds()    
+def plot_tether_limit_reel_speeds(step):
+    gamma_in_max_f_c,gamma_out_max_f_c,v_w = calculate_tether_limit_reel_speeds(step)    
     plt.plot(v_w, gamma_in_max_f_c, label = 'Reel-in speed')
     plt.plot(v_w,gamma_out_max_f_c,'--', label = 'Reel-out speed')
     plt.xlabel('Wind speed',fontsize = 16)
@@ -41,4 +45,4 @@ def plot_tether_limit_reel_speeds():
     plt.grid()
     plt.show()
 
-plot_tether_limit_reel_speeds()
+plot_tether_limit_reel_speeds(1000)
