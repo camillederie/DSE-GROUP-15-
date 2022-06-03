0aa8c1ce52e1a2b81fe25c3ef204a2cf47cb7270
Winter Oostwoud <ftw.oostwoud@gmail.com>
2022-06-03 15:32:15 +0200

    First part of iteration

--- Optimisation.py
+++ Optimisation.py
@@ -1,1 +1,16 @@
-#Titatovenaar
+#Optimisation of the Main System Parameters
+
+from Drum_and_tether_design import
+from Nominal_power_cycle
+from aero_main_function import main_aero_function
+
+
+
+A_proj = 16.65
+Points = 10000
+Kite_segments = 12
+N_split = 5
+AoA_range = np.arange(8, 10.5, 0.5)
+TAS = 32.5
+
+CL_average, CD_average, CL3_CD2_average, A_proj, Strut_area_av = main_aero_function(A_proj, Points, Kite_segments, N_split, AoA_range, TAS, Print=False)
